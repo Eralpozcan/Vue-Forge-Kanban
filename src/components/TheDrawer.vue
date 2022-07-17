a3528e60d39fbf34a26ce946b6141368df35eb5a
Eralpozcan <eralpozcans@gmail.com>
2022-07-17 20:04:24 +0300

    add function and css settings for TheDrawer.

--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -2,16 +2,61 @@
 import { Drawer, DrawerContent } from "@progress/kendo-vue-layout";
 import { useRouter } from 'vue-router';
 import { computed,ref} from 'vue';
+import router from "@/router";
 
+const selectedId = ref(0);
+const expanded = ref(false);
+const expandedIcons = computed (() => 
+    expanded.value ? 'k-i-arrow-left' : 'k-i-arrow-right'
+);
 
+const items = computed(() => [
+  {
+    text: "Boards",
+    icon: "k-i-set-column-position",
+    selected: true,
+    data: {
+      path: "/",
+    },
+  },
+  {
+    text: "Templates",
+    icon: "k-i-border-left",
+    data: {
+      path: "/templates",
+    },
+  },
+  {
+    text: "Settings",
+    icon: "k-i-gear",
+    data: {
+      path: "/settings",
+    },
+  },
+  {
+    text: "Collapse",
+    icon: expandedIcons.value,
+    data: {
+      action: () => (expanded.value = !expanded.value),
+    },
+  },
+]);
 
+function onSelect({ itemIndex}: { itemIndex: number }) {
+    const item = items.value[itemIndex]
+    if(item.data.path) router.push(item.data.path)
+    if (typeof item.data.action === 'function') item.data.action()
+}
 
 </script>
 
 <template>
-    <Drawer>
+    <Drawer class="h-[90vh]" :expanded="expanded" position="start" mode="push" :mini="true" :items="items"
+        @select="onSelect">
         <DrawerContent>
-            <router-view />
+            <div class="px-5">
+                <router-view />
+            </div>
         </DrawerContent>
     </Drawer>
 </template>
