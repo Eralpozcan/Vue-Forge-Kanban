59833dd451231e42b7f1fea741da1600b7b3821b
Eralpozcan <eralpozcans@gmail.com>
2022-07-21 23:45:23 +0300

    update

--- src/pages/boards/[id].vue
+++ src/pages/boards/[id].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AppImageDropzone from "../../components/AppImageDropzone.vue";
+import AppImageDropzone from "../../components/AppImageDropzone";
 </script>
 
 <template>
