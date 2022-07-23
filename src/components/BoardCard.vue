70ab2444c0bb6030bf7592e6a3493862122a2db1
Eralpozcan <eralpozcans@gmail.com>
2022-07-23 15:58:44 +0300

    add create draganddrop for kanban board

--- src/components/BoardCard.vue
+++ src/components/BoardCard.vue
@@ -1,34 +1,31 @@
 <script setup lang="ts">
 import {
-    Card as KCard,
-    CardTitle as KCardTitle,
-} from '@progress/kendo-vue-layout';
-import type { Board } from '@/types';
-defineProps <{
-    board: Board;
+  Card as KCard,
+  CardTitle as KCardTitle,
+} from "@progress/kendo-vue-layout";
+import type { Board } from "@/types";
+defineProps<{
+  board: Board;
 }>();
-const randomColor = "#" + Math.floor(Math.random() * 16777215).toString();
-
+const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 </script>
 <template>
-    <router-link :to="`/boards/${board.id}`" class="block w-96">
-        <KCard class ="m-5" style="background-color: ${randomColor}">
-            <AppImage
-                v-if="board.image"
-                :src="board.image?.downloadUrl"
-                width="384"
-                class = "aspect-video w-full"
-            />
-            <div
-                v-else
-                class = "aspect-video w-full"
-                :style="{backgroundColor: randomColor}"
-            ></div>
-            <KCardTitle class="p-2">
-                <span class="text-gray-700 text-xl">
-                    {{ board.title }}
-                </span>
-            </KCardTitle>
-        </KCard>
-    </router-link>
+  <router-link :to="`/boards/${board.id}`" class="block w-96">
+    <KCard>
+      <AppImage
+        v-if="board.image"
+        :src="board.image?.downloadUrl"
+        width="384"
+        class="w-full aspect-video"
+      />
+      <div
+        v-else
+        class="w-full aspect-video"
+        :style="{ backgroundColor: randomColor }"
+      ></div>
+      <KCardTitle class="p-2">
+        <span class="text-xl text-gray-700">{{ board.title }}</span>
+      </KCardTitle>
+    </KCard>
+  </router-link>
 </template>
